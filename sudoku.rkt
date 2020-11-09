c40dbf1b7006556b797719fad5cad1f994b16c69
holymiko <holymiko@fit.cvut.cz>
2020-11-09 22:57:28 +0100

    n-th column

--- sudoku.rkt
+++ sudoku.rkt
@@ -1,1 +1,16 @@
 #lang racket
+
+(define square
+  '((1 2 3)
+    (4 5 6)
+    (7 8 9)))
+
+(define (nth list index)
+  (if (null? list)
+      null
+      (if (= index 0)
+          (car list)
+          (nth (cdr list) (- index 1)))))
+
+(define (nth-column matrix index)
+  (map (lambda (row) (nth row index)) matrix))
