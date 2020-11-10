6b36a43ee078b18cd80912607d67f5a249ecacb7
holymiko <holymiko@fit.cvut.cz>
2020-11-10 22:45:26 +0100

    Complete size and duplicity check

--- sudoku.rkt
+++ sudoku.rkt
@@ -1,32 +1,61 @@
 #lang racket
 
-(define square0
-  '((1 2 3 4)
-    (4 3 6 5)
-    (7 8 9 9)
-    (7 8 9 6)))
-
-(define square
+(define s0
   '((1 2 3 4)
     (4 3 6 5)
     (0 8 0 0)
     (0 7 9 6)))
 
-(define square1
+(define s1
   '((1 2 3 4)
     (4 3 6 5)
     (7 8 9 6)
-    (5 7 1 2)))
+    (5 0 1 2)))
 
-(define square2
-  '((1 2 3 2 6)
-    (4 5 6 5 6)
-    (7 8 9 5 6)))
+(define s2
+  '((0 0 0 0 0 0 0 0 0)
+    (0 0 0 0 0 0 0 0 0)
+    (0 0 0 0 0 0 0 0 0)
+    (0 0 0 0 0 0 0 0 0)
+    (0 0 0 0 0 0 0 0 0)
+    (0 0 0 0 0 0 0 0 0)
+    (0 0 0 0 0 0 0 0 0)
+    (0 0 0 0 0 0 0 0 0)
+    (0 0 0 0 0 0 0 0 0)))
 
-(define square3
-  '((1 2 3 1 2 3 1 2 3)
-    (4 5 6 4 5 6 4 5 6)
-    (7 8 9 7 8 9 7 8 9)))
+(define s3
+  '((1 0 0  0 2 0  0 0 3)
+    (4 5 6  4 5 6  4 5 6)
+    (7 8 9  7 8 9  7 8 9)
+    
+    (0 0 0  1 2 3  1 2 3)
+    (4 5 6  0 0 0  4 5 6)
+    (7 8 9  7 8 9  0 0 0)
+    
+    (9 0 0  1 2 3  1 2 3)
+    (4 5 6  0 0 0  0 0 0)
+    (0 0 0  7 8 9  7 8 9)))
+
+(define s4
+  '((0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 0)
+    (0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 0)
+    (0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 0)
+    (0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 0)
+
+    (0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 0)
+    (0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 0)
+    (0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 0)
+    (0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 0)
+    
+    (0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 0)
+    (0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 0)
+    (0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 0)
+    (0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 0)
+
+    (0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 0)
+    (0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 0)
+    (0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 0)
+    (0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 0)))
 
 (define (nth list index)
   (if (null? list)
@@ -39,6 +68,7 @@
 (define (nth-column matrix index)
   (map (lambda (row) (nth row index)) matrix))
 
+#|\\\\\\\\\\\\\\SIZE CHECK//////////////////////////////////////////|#
 
 (define (rowSize matrix)
   (define sizeSqrt (sqrt (length (car matrix))))
@@ -62,11 +92,7 @@
   )
 )
 
-#|Checks size of matrix|#
-(define (matrixSizeCheck matrix)
-  (rowSize matrix)
-  (rowsCheck matrix (length (car matrix)) (length (car matrix)))
-  )
+#|\\\\\\\\\\\\\\DUPLICITY CHECK//////////////////////////////////////////|#
 
 (define (rowsDuplicateCheck matrix)
   (if (null? matrix)
@@ -76,8 +102,7 @@
                (rowsDuplicateCheck (cdr matrix))
                (error "Duplicate in row"))
            (rowsDuplicateCheck (cdr matrix)))
-      )
-  )
+      ))
 
 (define (columnCheck matrix acc)
   (if (= acc 0)
@@ -87,19 +112,77 @@
                (columnCheck matrix (- acc 1))
                (error "Duplicate in column"))
           (columnCheck matrix (- acc 1))
-          )
-      )
-  )
+          )))
 
 (define (columnDuplicateCheck matrix)
-  (columnCheck matrix (- (length (car matrix)) 1))
-  )
+  (columnCheck matrix (- (length (car matrix)) 1)))
 
-#|Checks matrix duplicities (excluding 0)|#
-#|Box check is missing|#
-(define (matrixDuplicateCheck matrix)
+
+(define (boxCheck matrix count)
+  (if (> count 0)
+      (if (check-duplicates (getBox matrix count))
+           (if (= (check-duplicates (getBox matrix count)) 0)
+               (boxCheck matrix (- count 1))
+               (error "Duplicate in box"))
+           (boxCheck matrix (- count 1)))
+      true))
+
+(define (boxDuplicateCheck matrix)
+  (boxCheck matrix (- (length (car matrix)) 1)))
+
+#|\\\\\\\\\\\\\\BOX EXTRACTING//////////////////////////////////////////|#
+
+#| cdr N times |#
+(define (matrixCdr matrix times)
+  (if (null? (cdr matrix))
+      (error "matrixCdr")
+      (if (<= times 0)
+          matrix
+          (matrixCdr (cdr matrix) (- times 1)))))
+
+(define (cutter matrix number)
+  (define sizeSqrt (sqrt (length (car matrix))))
+  (if (>= number sizeSqrt)
+      (cutter (matrixCdr matrix sizeSqrt) (- number sizeSqrt))
+      (extractBox (matrixCdr2 (reverse matrix) sizeSqrt) sizeSqrt '() number)))
+
+#| cdr until only box rows left|#
+(define (matrixCdr2 matrix sizeSqrt)
+  (if (> (length matrix ) sizeSqrt )
+      (matrixCdr2 (matrixCdr matrix sizeSqrt) sizeSqrt)
+      (reverse matrix)))
+
+#| Exctract box from given rows |#
+(define (extractBox matrix sizeSqrt new number)
+  (if (null? matrix)
+      new
+      (extractBox (cdr matrix)
+               sizeSqrt
+               (append new (reverse (list-tail
+                                     (reverse (list-tail (car matrix) (* number sizeSqrt)))
+                                     (- (length (list-tail (car matrix) (* number sizeSqrt))) sizeSqrt )
+                                     )))
+               number)))
+
+(define (getBox matrix number)
+  (if (< number 0)
+      (error "Looking for box smaller then zero")
+      (if (>= number (length (car matrix)))
+          (error "Looking for box out of range")
+          (cutter matrix number))))
+
+
+
+          
+
+#|Checks matrix size and duplicities (excluding 0)|#
+#|Number out of range check missing|#
+(define (matrixCheck matrix)
+  (rowSize matrix)
+  (rowsCheck matrix (length (car matrix)) (length (car matrix)))
   (rowsDuplicateCheck matrix)
   (columnDuplicateCheck matrix)
+  (boxDuplicateCheck matrix)
   )
           
 
