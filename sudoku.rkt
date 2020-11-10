f12ddf2fa9460dbe2bc92e46f3c347a204763db9
holymiko <holymiko@fit.cvut.cz>
2020-11-10 16:23:39 +0100

    Matrix size check

--- sudoku.rkt
+++ sudoku.rkt
@@ -1,14 +1,16 @@
 #lang racket
 
 (define square
-  '((1 2 3)
-    (4 5 6)
-    (7 8 9)))
+  '((1 2 3 4)
+    (4 5 6 5)
+    (7 8 9 6)
+    (7 8 9 6)))
 
 (define square1
   '((1 2 3 4)
     (4 5 6 5)
-    (7 8 9 6)))
+    (7 8 9 6)
+    (7 8 9 4)))
 
 (define square2
   '((1 2 3 2 6)
@@ -32,9 +34,37 @@
 
 #|Checks size of the first row|#
 (define (rowSize matrix)
-  (if (integer? (sqrt (length (car matrix))))
-      (if (< (sqrt (length (car matrix))) 2)
+  (define sizeSqrt (sqrt (length (car matrix))))
+  (if (integer? sizeSqrt)
+      (if (< sizeSqrt 2)
           (error "Too small")
           true)
       (error "Illegal size"))
   )
+
+(define (rowsCheck matrix n acc)
+  (if (= acc 0)
+      (if (null? matrix)
+          true
+          (error "Too many rows"))
+      (if (null? matrix)
+          (error "Not enought rows")
+          (if (= (length (car matrix)) n)
+              (rowsCheck (cdr matrix) n (- acc 1))
+              (error "Wrong size of row")
+          )
+      )
+  )
+)
+
+#|Checks size of matrix|#
+(define (matrixSizeCheck matrix)
+  (rowSize matrix)
+  (rowsCheck matrix (length (car matrix)) (length (car matrix)))
+  )
+
+
+
+
+
+
