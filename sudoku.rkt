626e9a18dc26084aad97024bf82d599d64fac478
holymiko <holymiko@fit.cvut.cz>
2020-11-10 17:24:14 +0100

    Duplicities in rows and columns check

--- sudoku.rkt
+++ sudoku.rkt
@@ -1,16 +1,22 @@
 #lang racket
 
+(define square0
+  '((1 2 3 4)
+    (4 3 6 5)
+    (7 8 9 9)
+    (7 8 9 6)))
+
 (define square
   '((1 2 3 4)
-    (4 5 6 5)
-    (7 8 9 6)
-    (7 8 9 6)))
+    (4 3 6 5)
+    (0 8 0 0)
+    (0 7 9 6)))
 
 (define square1
   '((1 2 3 4)
-    (4 5 6 5)
+    (4 3 6 5)
     (7 8 9 6)
-    (7 8 9 4)))
+    (5 7 1 2)))
 
 (define square2
   '((1 2 3 2 6)
@@ -29,16 +35,17 @@
           (car list)
           (nth (cdr list) (- index 1)))))
 
+#|Returns n-th column|#
 (define (nth-column matrix index)
   (map (lambda (row) (nth row index)) matrix))
 
-#|Checks size of the first row|#
+
 (define (rowSize matrix)
   (define sizeSqrt (sqrt (length (car matrix))))
   (if (integer? sizeSqrt)
       (if (< sizeSqrt 2)
           (error "Too small")
-          true)
+          true )
       (error "Illegal size"))
   )
 
@@ -51,9 +58,7 @@
           (error "Not enought rows")
           (if (= (length (car matrix)) n)
               (rowsCheck (cdr matrix) n (- acc 1))
-              (error "Wrong size of row")
-          )
-      )
+              (error "Wrong size of row")))
   )
 )
 
@@ -63,8 +68,40 @@
   (rowsCheck matrix (length (car matrix)) (length (car matrix)))
   )
 
+(define (rowsDuplicateCheck matrix)
+  (if (null? matrix)
+      true
+      (if (check-duplicates (car matrix))
+           (if (= (check-duplicates (car matrix)) 0)
+               (rowsDuplicateCheck (cdr matrix))
+               (error "Duplicate in row"))
+           (rowsDuplicateCheck (cdr matrix)))
+      )
+  )
 
+(define (columnCheck matrix acc)
+  (if (= acc 0)
+      true
+      (if (check-duplicates (nth-column matrix acc))
+          (if (= (check-duplicates (nth-column matrix acc)) 0)
+               (columnCheck matrix (- acc 1))
+               (error "Duplicate in column"))
+          (columnCheck matrix (- acc 1))
+          )
+      )
+  )
 
+(define (columnDuplicateCheck matrix)
+  (columnCheck matrix (- (length (car matrix)) 1))
+  )
+
+#|Checks matrix duplicities (excluding 0)|#
+#|Box check is missing|#
+(define (matrixDuplicateCheck matrix)
+  (rowsDuplicateCheck matrix)
+  (columnDuplicateCheck matrix)
+  )
+          
 
 
 
