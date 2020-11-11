b4573884271e208d7419c8f9c7090940d2cd0af4
holymiko <holymiko@fit.cvut.cz>
2020-11-11 19:31:43 +0100

    Row/Column filling (excluding box)

--- sudoku.rkt
+++ sudoku.rkt
@@ -12,29 +12,31 @@
     (2 0 0 1)
     (0 0 4 2)))
 
-(define s2
-  '((0 0 0 0 0 0 0 0 0)
-    (0 0 0 0 0 0 0 0 0)
-    (0 0 0 0 0 0 0 0 0)
-    (0 0 0 0 0 0 0 0 0)
-    (0 0 0 0 0 0 0 0 0)
-    (0 0 0 0 0 0 0 0 0)
-    (0 0 0 0 0 0 0 0 0)
-    (0 0 0 0 0 0 0 0 0)
-    (0 0 0 0 0 0 0 0 0)))
+(define s2                #| Legit sudoku from web |#
+  '((1 0 5  0 0 2  0 9 0)
+    (0 2 0  5 4 9  0 6 3)
+    (0 4 0  0 3 0  0 2 0)
+    
+    (0 6 0  0 1 0  0 3 0)
+    (4 0 0  9 2 8  0 0 1)
+    (5 0 1  0 6 0  0 0 9)
+    
+    (0 9 0  0 0 0  0 0 0)
+    (8 0 0  3 9 0  4 7 5)
+    (3 5 7  2 8 0  9 0 0)))
 
 (define s3
   '((1 0 0  0 2 0  0 0 3)
-    (4 5 6  4 5 6  4 5 6)
-    (7 8 9  7 8 9  7 8 9)
+    (4 0 6  4 5 6  4 5 6)
+    (7 0 9  6 8 9  5 8 9)
     
-    (0 0 0  1 2 3  1 2 3)
-    (4 5 6  0 0 0  4 5 6)
+    (0 6 0  0 2 3  6 2 3)
+    (4 7 6  5 0 0  0 5 6)
     (7 8 9  7 8 9  0 0 0)
     
-    (9 0 0  1 2 3  1 2 3)
-    (4 5 6  0 0 0  0 0 0)
-    (0 0 0  7 8 9  7 8 9)))
+    (9 0 0  8 2 3  9 2 3)
+    (4 9 6  0 0 0  0 0 0)
+    (0 0 0  0 8 9  7 8 9)))
 
 (define s4
   '((0 0 0 0  0 0 0 0  0 0 0 0  0 0 0 0)
@@ -65,7 +67,7 @@
           (nth (cdr list) (- index 1)))))
 
 #|Returns n-th column|#
-(define (nth-column matrix index)
+(define (getColumn matrix index)
   (map (lambda (row) (nth row index)) matrix))
 
 #|\\\\\\\\\\\\\\SIZE CHECK//////////////////////////////////////////|#
@@ -125,7 +127,7 @@
 (define (columnCheck matrix acc)
   (if (= acc 0)
       true
-      (if (check-duplicates (remove* (list 0) (nth-column matrix acc)))
+      (if (check-duplicates (remove* (list 0) (getColumn matrix acc)))
           (error "Duplicate in column")
           (columnCheck matrix (- acc 1))
           )))
@@ -194,7 +196,49 @@
   (columnDuplicateCheck matrix)
   (boxDuplicateCheck matrix)
   )
-          
+
+#| Numbers that can be putted instead of 0 on this line |#
+(define (rowPossible line)
+  (remove* line (range 1 (+ (length line) 1))))
+
+#| Numbers that can be putted instead of 0 on this position |#        #| Doesnt include box numbers |#
+(define (positionPossible column rowPossible)
+  (remove* column rowPossible))
+ 
+#| Returns new line with first possible 0 filled with number |#
+(define (newRow matrix line zeroIndex)
+  (define rowPos (rowPossible line))
+  (if (null? zeroIndex)
+      null
+      (if (null? (positionPossible (getColumn matrix (car zeroIndex)) rowPos))
+          (error "Fatal error - Position of 0 has no filling possibilities")
+          (if (> (length (positionPossible (getColumn matrix (car zeroIndex)) rowPos)) 1)
+              (newRow matrix line (cdr zeroIndex))
+              (list-set line (car zeroIndex) (car (positionPossible (getColumn matrix (car zeroIndex)) rowPos))) ))))
+
+#| Repeats rowIter from begging always when any row is overwritten (0 -> number) |#
+(define (rowIter fullMatrix n)
+  (if (>= n (length fullMatrix))
+      fullMatrix
+      (if (null? (newRow fullMatrix (list-ref fullMatrix n) (indexes-where (list-ref fullMatrix n) zero?)) )
+          (rowIter fullMatrix (+ n 1))
+          (rowIter (list-set
+                            fullMatrix #| Matrix to be editted |#
+                            n          #| Position |#
+                            (newRow    #| New line |#
+                                   fullMatrix 
+                                   (list-ref fullMatrix n) #| Line |#
+                                   (indexes-where (list-ref fullMatrix n) zero?)  #| List of zero indexes |#
+                            )
+                    ) 0 )
+       )))
+
+(define (rowFill matrix)
+  (rowIter matrix 0))
+
+
+
+
 
 
 
